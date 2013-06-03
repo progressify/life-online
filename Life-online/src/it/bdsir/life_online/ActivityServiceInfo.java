e3e1e2c0b124f64e2758197d06c86e27e5f51e8a
a.porcelli90@gmail.com <a.porcelli90@gmail.com@959e2c7c-e3e7-4395-cea6-e2b68b3aab2e>
2013-06-03 07:53:02 +0000

    ottimizzato il codice del datapicker sistemati gli id dell'activity service info

--- Life-online/src/it/bdsir/life_online/ActivityServiceInfo.java
+++ Life-online/src/it/bdsir/life_online/ActivityServiceInfo.java
@@ -3,27 +3,26 @@ package it.bdsir.life_online;
 import java.util.Calendar;
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
 public class ActivityServiceInfo extends Activity implements OnClickListener{
 
 	private EditText editDataN; 
-	private Dialog dialog;
 	private Calendar calen=Calendar.getInstance();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_activity_service_info);
-		editDataN=(EditText)findViewById(R.id.editText3);
+		editDataN=(EditText)findViewById(R.id.edit_data_nascita);
 		editDataN.setInputType(InputType.TYPE_NULL);
 		editDataN.setFocusable(false);
 		editDataN.setOnClickListener(this);
@@ -31,39 +30,24 @@ public class ActivityServiceInfo extends Activity implements OnClickListener{
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_activity_service_info, menu);
 		return true;
 	}
 
 	@Override
 	public void onClick(View v) {
-		dialog = new Dialog(this);
-		dialog.setContentView(R.layout.date_layout);
-		dialog.setCancelable(true);
-		//dialog.setTitle(getResources().getString(R.string.popup_preventivi_data));
-		DatePicker dp = (DatePicker)dialog.findViewById(R.id.datePicker1);
-		Button buttonSet=(Button)dialog.findViewById(R.id.buttonSet);
-		buttonSet.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				if (v.getId()== R.id.buttonSet){
-					onDateSet();
+		if (v.getId()==R.id.edit_data_nascita){
+			int year = calen.get(Calendar.YEAR);
+			int month = calen.get(Calendar.MONTH);
+			int day = calen.get(Calendar.DAY_OF_MONTH);
+			DatePickerDialog dialogs = new DatePickerDialog(this,new OnDateSetListener() {
+				public void onDateSet(DatePicker view, int year, int monthOfYear,int dayOfMonth) {
+					calen.set(year, monthOfYear, dayOfMonth);
+					editDataN.setText(calen.get(Calendar.DAY_OF_MONTH)+"-"+(calen.get(Calendar.MONTH)+1)+"-"+calen.get(Calendar.YEAR));
 				}
-			}
-		});
-		int year = calen.get(Calendar.YEAR);
-		int month = calen.get(Calendar.MONTH);
-		int day = calen.get(Calendar.DAY_OF_MONTH);
-		dp.init(year, month, day, new DatePicker.OnDateChangedListener() {
-			public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-				calen.set(year, monthOfYear, dayOfMonth);
-			}
-		});
-		dialog.show();
-	}
-	
-	private void onDateSet(){
-		editDataN.setText(calen.get(Calendar.DAY_OF_MONTH)+"-"+(calen.get(Calendar.MONTH)+1)+"-"+calen.get(Calendar.YEAR));
-		dialog.dismiss();
+			}, year, month, day);
+			dialogs.setTitle("Imposta Data");
+			dialogs.show();
+		}
 	}
 }
