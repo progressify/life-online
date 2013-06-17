d17d9c411f197731162463482d45a7c535aef53c
francesco.dinardo85 <francesco.dinardo85@959e2c7c-e3e7-4395-cea6-e2b68b3aab2e>
2013-06-17 18:35:25 +0000

    Migliorie Estetiche. Aggiunto sfondo migliore e migliorati i pulsanti.

--- Life-online/gen/it/bdsir/life_online/R.java
+++ Life-online/gen/it/bdsir/life_online/R.java
@@ -36,12 +36,13 @@ public final class R {
         public static final int size_spinner=0x7f050003;
     }
     public static final class drawable {
-        public static final int background_green=0x7f020000;
-        public static final int background_orange=0x7f020001;
-        public static final int background_red=0x7f020002;
-        public static final int bgtail=0x7f020003;
-        public static final int bitmap=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
+        public static final int background_button=0x7f020000;
+        public static final int background_green=0x7f020001;
+        public static final int background_orange=0x7f020002;
+        public static final int background_red=0x7f020003;
+        public static final int bgtail=0x7f020004;
+        public static final int bitmap=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0028;
